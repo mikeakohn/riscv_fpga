6b3b1648282f5185236682056b19bd49c1ab53e5
Michael Kohn <mike@mikekohn.net>
2023-12-20 18:26:52 -0600

    Update eeprom module.

--- src/eeprom.v
+++ src/eeprom.v
@@ -1,4 +1,4 @@
-// RISC-V FPGA Soft Processor
+// Ferrati F100-L FPGA Soft Processor
 //  Author: Michael Kohn
 //   Email: mike@mikekohn.net
 //     Web: https://www.mikekohn.net/
@@ -22,17 +22,20 @@ module eeprom
   output reg eeprom_clk,
   output reg eeprom_di,
   input  eeprom_do,
-  output reg ready,
+  output ready,
   output reg [7:0] data_out
 );
 
-reg [2:0] state = 0;
-reg [2:0] clock_div;
-wire clk;
-assign clk = clock_div[2];
+// Data sheet says 2.7v to 5.5v has a max speed of 1MHz. raw_clk should be
+// 12MHz, so divide by 16.
+//reg [4:0] clock_div;
+reg [2:0] delay = 0;
+//wire clk;
+//assign clk = clock_div[4];
 
 reg [13:0] command;
 reg [3:0] count;
+//reg running = 0;
 
 parameter STATE_IDLE           = 0;
 parameter STATE_SEND_ADDRESS_0 = 1;
@@ -42,13 +45,19 @@ parameter STATE_READ_DATA_0    = 4;
 parameter STATE_READ_DATA_1    = 5;
 parameter STATE_FINISH         = 6;
 
+reg [2:0] state = STATE_FINISH;
+
+assign ready = state == STATE_IDLE;
+
 // To run the AT93C86A at a speed slower than 2MHz, divide the clock down.
+/*
 always @(posedge raw_clk) begin
   clock_div <= clock_div + 1;
 end
+*/
 
 // State machine for reading the SPI-like EEPROM.
-always @(posedge clk) begin
+always @(posedge raw_clk) begin
   case (state)
     STATE_IDLE:
       begin
@@ -57,35 +66,45 @@ always @(posedge clk) begin
           command[13:11] <= 3'b110;
           command[10:0] <= address;
           count <= 14;
-          ready <= 0;
           eeprom_cs <= 1;
           state <= STATE_SEND_ADDRESS_0;
         end else begin
           eeprom_cs <= 0;
           eeprom_di <= 0;
           eeprom_clk <= 0;
-          ready <= 1;
         end
       end
     STATE_SEND_ADDRESS_0:
       begin
-        // Clock out 3 bits of command and 10 bytes of
+        // Clock out 3 bits of command and 11 bits of
         // address to the EEPROM.
-        count <= count - 1;
-        eeprom_di <= command[13];
-        eeprom_clk <= 0;
-        state <= STATE_SEND_ADDRESS_1;
+        case (delay)
+          0:
+            begin
+              count <= count - 1;
+              eeprom_di <= command[13];
+              command[13:1] <= command[12:0];
+              eeprom_clk <= 0;
+            end
+          7:
+            state <= STATE_SEND_ADDRESS_1;
+        endcase
+
+        delay <= delay + 1;
       end
     STATE_SEND_ADDRESS_1:
       begin
         eeprom_clk <= 1;
 
-        if (count == 0) begin
-          state <= STATE_READ_START;
-        end else begin
-          command[13:1] <= command[12:0];
-          state <= STATE_SEND_ADDRESS_0;
+        if (delay == 7) begin
+          if (count == 0) begin
+            state <= STATE_READ_START;
+          end else begin
+            state <= STATE_SEND_ADDRESS_0;
+          end
         end
+
+        delay <= delay + 1;
       end
     STATE_READ_START:
       begin
@@ -97,21 +116,37 @@ always @(posedge clk) begin
     STATE_READ_DATA_0:
       begin
         // Clock in 8 bits of data from the EEPROM.
-        count <= count - 1;
-        data_out[7:1] <= data_out[6:0];
-        eeprom_clk <= 1;
-        state <= STATE_READ_DATA_1;
+        case (delay)
+          1:
+            begin
+              count <= count - 1;
+              data_out[7:1] <= data_out[6:0];
+              eeprom_clk <= 1;
+            end
+          7:
+            state <= STATE_READ_DATA_1;
+        endcase
+
+        delay <= delay + 1;
       end
     STATE_READ_DATA_1:
       begin
-        data_out[0] <= eeprom_do;
         eeprom_clk <= 0;
 
-        if (count == 0) begin
-          state <= STATE_FINISH;
-        end else begin
-          state <= STATE_READ_DATA_0;
-        end
+        case (delay)
+          0:
+            data_out[0] <= eeprom_do;
+          7:
+            begin
+              if (count == 0) begin
+                state <= STATE_FINISH;
+              end else begin
+                state <= STATE_READ_DATA_0;
+              end
+            end
+        endcase
+
+        delay <= delay + 1;
       end
     STATE_FINISH:
       begin
